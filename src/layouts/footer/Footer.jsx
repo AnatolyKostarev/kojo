3f84f8a95f3dd62c5e711d4de7c063298c928ad8
Anatoly <anatolykostarev@gmail.com>
2022-01-07 14:37:20 +0300

    initial app structure

---
+++ src/layouts/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <div>
+      <h1 className={styles.title}>FOOTER</h1>
+    </div>
+  );
+}
