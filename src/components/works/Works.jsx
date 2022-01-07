10a58d941b54d58d82b752a05d80eacd751a2cc8
Anatoly <anatolykostarev@gmail.com>
2022-01-07 18:55:32 +0300

    Refactor Works component code. Add section, div.container

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,8 +3,12 @@ import styles from "./Works.module.css";
 
 export default function Works() {
   return (
-    <div>
-      <h1 className={styles.title}>WORKS ME</h1>
-    </div>
+    <>
+      <section className={styles.works}>
+        <div className="container">
+          <h1 className={styles.title}>WORKS</h1>
+        </div>
+      </section>
+    </>
   );
 }
