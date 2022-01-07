d552951da0f7959129ec2edabf215f9a45250a1f
Anatoly <anatolykostarev@gmail.com>
2022-01-07 21:37:33 +0300

    Create Footer Functional Component

--- src/layouts/footer/Footer.jsx
+++ src/layouts/footer/Footer.jsx
@@ -2,9 +2,36 @@ import React from "react";
 import styles from "./Footer.module.css";
 
 export default function Footer() {
+  const footerNav = [
+    { social: "/ facebook", href: "https://facebook.com" },
+    { social: "/ instagram", href: "https://instagram.com" },
+    { social: "/ youtube", href: "https://youtube.ru" },
+  ];
+
   return (
-    <div>
-      <h1 className={styles.title}>FOOTER</h1>
-    </div>
+    <>
+      <footer>
+        <div className={styles.container}>
+          <div className={styles.footer}>
+            <div className={styles.social}>
+              <p className={styles.title}>socials:</p>
+              <ul className={styles.list}>
+                {footerNav.map((elem, index) => (
+                  <li className={styles.item} key={index}>
+                    <a href={elem.href} className={styles.links}>
+                      {elem.social}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className={styles.copyright}>
+              {new Date().getFullYear()} &copy; "kojo" studio
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
   );
 }
