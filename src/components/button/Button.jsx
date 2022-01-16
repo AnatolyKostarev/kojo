c667970a60767d93a11664a49d84dccfeb6d7729
Anatoly <anatolykostarev@gmail.com>
2022-01-16 03:21:21 +0300

    Add attr modificator for Button component for positioning

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import styles from "./Button.module.css";
 
-export default function Button({ title }) {
+export default function Button({ title, modificator }) {
   return (
     <>
-      <a className={styles.btn} href="/" data-text={title}>
+      <a
+        className={styles.btn}
+        data-btn={modificator}
+        href="#"
+        data-text={title}
+      >
         {title}
       </a>
     </>
