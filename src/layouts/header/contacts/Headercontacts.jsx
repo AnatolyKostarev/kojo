4897c0a94548fc208540a198574375cfc79db15c
Anatoly <anatolykostarev@gmail.com>
2022-01-16 03:26:19 +0300

    Add Headercontacts component

---
+++ src/layouts/header/contacts/Headercontacts.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Headercontacts.module.css";
+
+export default function Headercontacts({ title }) {
+  return (
+    <>
+      <header id={styles.header}>
+        <div className={styles.container}>
+          <div className={styles.header}>
+            <div className={styles.header__logo}>
+              <Link to="/">
+                <div className={styles.header__img}>
+                  <img
+                    src="img/header/icon-logo.svg"
+                    alt="ZIPL"
+                    className={styles.logo}
+                  />
+                </div>
+              </Link>
+              <h1 className={styles.title}>{title}</h1>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
