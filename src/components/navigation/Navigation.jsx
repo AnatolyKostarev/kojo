412c5d3ca71e65fbcb630640f8a61a29259871ef
Anatoly <anatolykostarev@gmail.com>
2022-01-19 16:06:37 +0300

    Change nav logic

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,25 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./Navigation.module.css";
 
 export default function Navigation() {
+  let navSideBar = [
+    { linkto: "/", title: "Главная" },
+    { linkto: "/works", title: "Работы" },
+    { linkto: "/services", title: "Услуги" },
+    { linkto: "/contacts", title: "Контакты" },
+  ];
+
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Главная</Link>
-          </li>
-          <li>
-            <Link to="/works">Работы</Link>
-          </li>
-          <Link to="/services">
-            <li>Услуги</li>
-          </Link>
-          <Link to="/contacts">
-            <li>Контакты</li>
-          </Link>
-        </ul>
-      </nav>
+      <aside className={styles.nav__sidebar}>
+        <nav>
+          <ul>
+            {navSideBar.map((elem, index) => (
+              <li key={index}>
+                <Link to={elem.linkto}>{elem.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </aside>
     </>
   );
 }
