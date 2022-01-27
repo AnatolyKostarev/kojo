b4b27a707950fa87e083056908485b3e727901a6
Anatoly <anatolykostarev@gmail.com>
2022-01-27 19:04:49 +0300

    comment link transition-group animation lib

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -5,27 +5,24 @@ import AboutUs from "./about/aboutUs";
 import MainForm from "./form/form.jsx";
 import Footer from "./footer/footer.jsx";
 import { Header2 } from "../../layouts/header/homepage/headerMAx/Header2";
-import { Transition } from 'react-transition-group';
-
-
-
+// import { Transition } from 'react-transition-group';
 
 export default function Homepage() {
   // for header
   let [header, setHeader] = useState(true);
-  const hideHeader = () => { setHeader(false) };
-  const showHeader = () => { setHeader(true) };
+  const hideHeader = () => {
+    setHeader(false);
+  };
+  const showHeader = () => {
+    setHeader(true);
+  };
   // transition group
   //  let [state, setState] = useState(false);
 
-
   return (
     <>
-
       <div className={style.homepage}>
-
         {header && <Header2 />}
-
 
         {/* <Header2 /> */}
         <Title />
