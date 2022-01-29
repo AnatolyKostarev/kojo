7f58c56f91da3feee2e3786aa6f5964ba7264aeb
Aleksandr <tchistyakov1987@gmail.com>
2022-01-29 15:42:12 +0300

    validation form

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,46 +1,110 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import Button from "../button/Button";
 import styles from "./Form.module.css";
 
 export default function Form() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm({
+    mode: "onBlur",
+  });
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+  };
+
   return (
-    <form className={styles.form}>
-      <div className={styles.required}>
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+      <div className={styles.borders}>
         <input
+          name="Feedback[name]"
+          {...register("name", {
+            required: "Это поле обязательно",
+            minLength: {
+              value: 2,
+              message: "Минимум 2 символа",
+            },
+            maxLength: {
+              value: 50,
+              message: "Максимум 50 символов",
+            },
+          })}
           id="feedback-name"
           className={styles.input}
-          type="text"
-          name="Feedback[name]"
           placeholder="Имя"
-          minLength="2"
-          maxLength="50"
-          required
         />
-        {/* <p>Это поле обязательно</p> */}
+        <div className={styles.focus}>
+          {errors?.name && <p>{errors?.name?.message || "Error!"}</p>}
+        </div>
       </div>
-      <div>
+      <div className={styles.borders}>
         <input
+          name="Feedback[email]"
+          {...register("email", {
+            required: "Это поле обязательно",
+            maxLength: {
+              value: 50,
+              message: "Максимум 50 символов",
+            },
+            pattern: {
+              value:
+                /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/,
+              message: "Недопустимый формат email",
+            },
+          })}
           id="feedback-email"
           className={styles.input}
           type="email"
-          name="Feedback[email]"
           placeholder="Email"
-          required
         />
-        {/* <p>Это поле обязательно</p> */}
+        <div className={styles.focus}>
+          {errors?.email && <p>{errors?.email?.message || "Error!"}</p>}
+        </div>
       </div>
-      <div>
+      <div className={styles.borders}>
         <textarea
+          name="Feedback[message]"
+          {...register("message", {
+            required: "Это поле обязательно",
+            minLength: {
+              value: 10,
+              message: "Минимум 10 символа",
+            },
+            maxLength: {
+              value: 500,
+              message: "Максимум 500 символов",
+            },
+          })}
           id="feedback-message"
           className={styles.textarea}
-          name="Feedback[message]"
           placeholder="Сообщение"
-          minLength="10"
-          maxLength="500"
-          required
         ></textarea>
-        {/* <p>Это поле обязательно</p> */}
+        <div className={styles.focus}>
+          {errors?.message && <p>{errors?.message?.message || "Error!"}</p>}
+        </div>
       </div>
+      <label className={styles.consent}>
+        <input
+          name="Feedback[consent]"
+          type="checkbox"
+          {...register("consent", {
+            required: "И это тоже",
+          })}
+          id="feedback-consent"
+          className={styles.checkbox}
+        />
+        <span className={styles.checkfake}></span>
+        <span className={styles.policy_text}>
+          Я соглашаюсь с политикой о <a>персональных данных</a>
+        </span>
+        <div className={styles.focus_consent}>
+          {errors?.consent && <p>{errors?.consent?.message || "Error!"}</p>}
+        </div>
+      </label>
       <div className={styles.submitWrapper}>
         <Button title="Отправить" addStyle="form_btn" />
       </div>
