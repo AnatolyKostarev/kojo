5548a4722c721ce3ff7f2a9399272bf939151a55
Anatoly <anatolykostarev@gmail.com>
2022-01-31 20:12:19 +0300

    Reformate css code by Prettier

--- src/components/homepage/form/form.jsx
+++ src/components/homepage/form/form.jsx
@@ -1,32 +1,33 @@
 import style from "./form.module.css";
 import "../scrollSpy.css";
-import Form from '../../form/Form.jsx';
+import Form from "../../form/Form.jsx";
 
-function MainForm({hideHeader, showHeader}) {
-
+function MainForm({ hideHeader, showHeader }) {
   return (
     <>
-      <section className={style.form} id="form" onMouseOver={hideHeader} onMouseOut={showHeader}>
+      <section
+        className={style.form}
+        id="form"
+        onMouseOver={hideHeader}
+        onMouseOut={showHeader}
+      >
         <div className={style.mainForm}>
           <div className={style.mainBlock}>
             <div className={style.blockTitle}>
               <div>SEND US A</div>
-              <div>REQUEST TO
-</div>
+              <div>REQUEST TO</div>
               <div>CREATE </div>
               <div>UNIQUE </div>
               <div>PROJECT </div>
-              <div>FOR YOU
- </div>
+              <div>FOR YOU</div>
             </div>
-            {/* <form className={style.blockFrom}> */}
             <div className={style.blockFrom}>
               <p>
-              HIRE US NOW - WE'RE HERE TO ASSIST YOU. DROP US A LINE, AND WE'LL GET BACK TO YOU IN A BLINK OF AN EYE.
+                HIRE US NOW - WE'RE HERE TO ASSIST YOU. DROP US A LINE, AND
+                WE'LL GET BACK TO YOU IN A BLINK OF AN EYE.
               </p>
-            <Form/>
+              <Form />
             </div>
-            {/* </form> */}
           </div>
         </div>
       </section>
