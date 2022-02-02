bbff869296a846e07d02a18d174b5ca68df895a4
Anatoly <anatolykostarev@gmail.com>
2022-02-02 19:19:04 +0300

    Refactor footerHomepage comp

--- src/components/homepage/footer/footerHomepage/footerHomepage.jsx
+++ src/components/homepage/footer/footerHomepage/footerHomepage.jsx
@@ -1,46 +1,51 @@
 import React from "react";
-import styles from './footerHomepage.module.css'
-import '../../scrollSpy.css'
-export default function FooterHomepage({hideHeader,showHeader}) {
-    const footerWorksNav = [
-        { social: "/ фэйсбук", href: "https://facebook.com" },
-        { social: "/ инстаграм", href: "https://instagram.com" },
-        { social: "/ вконтакте", href: "https://vk.com" },
-    ];
-    return (
-        <>
-            <footer className={styles.footerHomepage} onMouseOver={hideHeader}
-        onMouseOut={showHeader}>
-                <div className={styles.container}>
-                    <div className={styles.footer}>
-                        <div className={styles.social}>
-                            <p className={styles.title}>мы в сети:</p>
-                            <ul className={styles.list}>
-                                {footerWorksNav.map((elem, index) => (
-                                    <li className={styles.item} key={index}>
-                                        <div className={styles.roll__link}>
-                                            <a
-                                                href={elem.href}
-                                                className={styles.link__top}
-                                                target="_blank"
-                                                rel="noreferrer"
-                                            >
-                                                {elem.social}
-                                            </a>
-                                            <a
-                                                href={elem.href}
-                                                className={styles.link__bottom}
-                                                target="_blank"
-                                                rel="noreferrer"
-                                            >
-                                                {elem.social}
-                                            </a>
-                                        </div>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                        {/* <div className="blockSpy">
+import styles from "./footerHomepage.module.css";
+import "../../scrollSpy.css";
+
+export default function FooterHomepage({ hideHeader, showHeader }) {
+  const footerWorksNav = [
+    { social: "/ фэйсбук", href: "https://facebook.com" },
+    { social: "/ инстаграм", href: "https://instagram.com" },
+    { social: "/ вконтакте", href: "https://vk.com" },
+  ];
+
+  return (
+    <>
+      <footer
+        className={styles.footerHomepage}
+        onMouseOver={hideHeader}
+        onMouseOut={showHeader}
+      >
+        <div className={styles.container}>
+          <div className={styles.footer}>
+            <div className={styles.social}>
+              <p className={styles.title}>мы в сети:</p>
+              <ul className={styles.list}>
+                {footerWorksNav.map((elem, index) => (
+                  <li className={styles.item} key={index}>
+                    <div className={styles.roll__link}>
+                      <a
+                        href={elem.href}
+                        className={styles.link__top}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        {elem.social}
+                      </a>
+                      <a
+                        href={elem.href}
+                        className={styles.link__bottom}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        {elem.social}
+                      </a>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            {/* <div className="blockSpy">
                             <nav className={styles.scrollSpy}>
                                 <a href="#title" id="title_btn"></a>
 
@@ -50,12 +55,12 @@ export default function FooterHomepage({hideHeader,showHeader}) {
                                 <span id="indicator"></span>
                             </nav>
                         </div> */}
-                        <div className={styles.copyright}>
-                            {new Date().getFullYear()} &copy; "kojo" студия
-                        </div>
-                    </div>
-                </div>
-            </footer>
-        </>
-    );
+            <div className={styles.copyright}>
+              {new Date().getFullYear()} &copy; "kojo" студия
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 }
