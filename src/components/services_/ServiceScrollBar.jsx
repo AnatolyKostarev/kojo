a4c9dc1e4a842c912318a2b56707f751835893ca
Anatoly <anatolykostarev@gmail.com>
2022-02-15 03:48:09 +0300

    Add scrolbar icon

---
+++ src/components/services_/ServiceScrollBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Services.module.css";
+
+export const ServiceScrollBar = () => {
+  return (
+    <div className={styles.service__scroll_bar}>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+};
