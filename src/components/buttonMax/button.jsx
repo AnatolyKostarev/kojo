92701f7af1212de5b3e0b9fef3f51a735f98c5c6
Anatoly <anatolykostarev@gmail.com>
2022-02-15 15:15:52 +0300

    Reformat code by Prettier

--- src/components/buttonMax/button.jsx
+++ src/components/buttonMax/button.jsx
@@ -1,14 +1,10 @@
-import style from './button.module.css';
-
+import style from "./button.module.css";
 
 function Button() {
-
-
-    return (
-        <a href='#form' id={style.anchor_form}>
-            <button id={style.btn}>
-            </button>
-        </a>
-    )
+  return (
+    <a href="#form" id={style.anchor_form}>
+      <button id={style.btn}></button>
+    </a>
+  );
 }
 export default Button;
