e758bc1e468730462295688e84f2d2b3e95f9f62
Anatoly <anatolykostarev@gmail.com>
2022-02-20 17:34:38 +0300

    Add props to close Mobile menu. Add event listener click

--- src/components/navigation/NavigationMobile.jsx
+++ src/components/navigation/NavigationMobile.jsx
@@ -4,14 +4,19 @@ import { navSideBar } from "../../store";
 import styles from "./NavigationMobile.module.css";
 import Button from "../button/Button";
 
-export default function NavigationMobile() {
+export default function NavigationMobile({ toggleMobileMenu }) {
   return (
     <>
       <div className={styles.menu}>
         <nav className={styles.nav}>
           <ul className={styles.nav__list}>
             {navSideBar.map((elem, index) => (
-              <Link className={styles.nav__item} to={elem.linkto} key={index}>
+              <Link
+                className={styles.nav__item}
+                to={elem.linkto}
+                key={index}
+                onClick={toggleMobileMenu}
+              >
                 <li className={styles.nav__items} data-text={elem.title}>
                   {elem.title}
                 </li>
