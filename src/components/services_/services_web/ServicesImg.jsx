d81703e058317cb60292c6c24aebbad7f719235c
Anatoly <anatolykostarev@gmail.com>
2022-02-23 20:51:13 +0300

    Add img wraper

--- src/components/services_/services_web/ServicesImg.jsx
+++ src/components/services_/services_web/ServicesImg.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import styles from "./ServicesWeb.module.css";
 
 export const ServicesImg = () => {
-  return <div className={styles.service__img}></div>;
+  return (
+    <div className={styles.service__img_wraper}>
+      <div className={styles.service__img}></div>
+    </div>
+  );
 };
