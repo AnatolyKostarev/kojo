b9fc78d154f2e6227047ad3cb34f932cf768a451
Anatoly <anatolykostarev@gmail.com>
2022-02-24 17:43:58 +0300

    Fix

---
+++ src/components/services_/services_mobile/ServiceMobileNavBar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./ServiceMobileNavBar.css";
+
+const ServiceMobileNavBar = () => {
+  return (
+    <div className="service__mobile_nav">
+      <ul>
+        <li>
+          <a href="#service">веб</a>
+        </li>
+        <li>
+          <a href="#service__rule">правила</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default ServiceMobileNavBar;
