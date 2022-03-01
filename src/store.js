daaa7a83e418a13d0a2147779603afa08e081cb0
Anatoly <anatolykostarev@gmail.com>
2022-03-01 14:53:44 +0300

    Change vars, delete comments

--- src/store.js
+++ src/store.js
@@ -42,62 +42,6 @@ export const worksItem = [
     height: "830",
     alt: "Cтудия KOJO",
   },
-  // {
-  //   imagePosition: "right",
-  //   buttonPosition: "left",
-  //   actionPosition: "left",
-  //   title: "летундра. приложение",
-  //   counter: "наша гордость #",
-  //   desc: "Удалить!!!",
-  //   action: "UI/UX",
-  //   href: "https://www.behance.net/gallery/65252599/Letundra-App-UIUX",
-  //   src: "/img/works/image_works-4.webp",
-  //   width: "720",
-  //   height: "720",
-  //   alt: "letundra",
-  // },
-  // {
-  //   imagePosition: "left",
-  //   buttonPosition: "right",
-  //   actionPosition: "right",
-  //   title: "icv шаблоны",
-  //   counter: "наша гордость #",
-  //   desc: "онлайн магазин шаблонов резюме",
-  //   action: "дизайн & код",
-  //   href: "https://icvtemplates.com/",
-  //   src: "/img/works/image_works-5.webp",
-  //   width: "840",
-  //   height: "830",
-  //   alt: "icv",
-  // },
-  // {
-  //   imagePosition: "right",
-  //   buttonPosition: "left",
-  //   actionPosition: "left",
-  //   title: "franklead",
-  //   counter: "наша гордость #",
-  //   desc: "сайт электронной музыки",
-  //   action: "дизайн & код",
-  //   href: "http://franklead.com/",
-  //   src: "/img/works/image_works-6.webp",
-  //   width: "720",
-  //   height: "720",
-  //   alt: "franklead",
-  // },
-  // {
-  //   imagePosition: "left",
-  //   buttonPosition: "right",
-  //   actionPosition: "right",
-  //   title: "letusbuy ибица",
-  //   counter: "наша гордость #",
-  //   desc: "сайт агентства недвижимости на ибице",
-  //   action: "дизайн & код",
-  //   href: "https://www.behance.net/gallery/65436315/LetUsBuy",
-  //   src: "/img/works/image_works-7.webp",
-  //   width: "840",
-  //   height: "830",
-  //   alt: "letusbuy",
-  // },
 ];
 
 // Array of object for Navigation.jsx component
@@ -111,16 +55,27 @@ export const navSideBar = [
 // Array of object for Footerworks.jsx component
 
 export const footerWorksNav = [
-  { social: "/ фэйсбук", href: "https://facebook.com" },
-  { social: "/ инстаграм", href: "https://instagram.com" },
+  {
+    social: "/ facebook",
+    href: "https://www.facebook.com/Saint-Code-Bootcamp-114797186729898",
+  },
+  {
+    social: "/ instagram",
+    href: "https://www.instagram.com/saint_code_bootcamp",
+  },
 ];
 
 // Array of object for Contacts.jsx component
 
 export const socialList = [
-  { social: "Фэйсбук", href: "https://facebook.com/" },
-  { social: "Инстаграм", href: "https://instagram.com/" },
-  // { social: "Беханс", href: "https://www.behance.net/" },
+  {
+    social: "facebook",
+    href: "https://www.facebook.com/Saint-Code-Bootcamp-114797186729898",
+  },
+  {
+    social: "instagram",
+    href: "https://www.instagram.com/saint_code_bootcamp",
+  },
 ];
 
 export const contactsList = [
