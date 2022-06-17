cd4724796e7b77c44f485dcea17e0cf14b19e69d
Anatoly <anatolykostarev@gmail.com>
2022-06-17 14:19:16 +0300

    Fix styles

--- src/components/homepage/form/form.jsx
+++ src/components/homepage/form/form.jsx
@@ -5,24 +5,23 @@ import Form from "../../form/Form.jsx";
 function MainForm({ hideHeader, showHeader }) {
   return (
     <>
-      <section
-        className={style.form}
-        id="form"
-        onMouseOver={hideHeader}
-      >
+      <section className={style.form} id="form" onMouseOver={hideHeader}>
         <div className={style.mainForm}>
           <div className={style.mainBlock}>
             <div className={style.blockTitle}>
+              <div>ХОЧЕШЬ</div>
+              <div>ПОГОВОРИТЬ?</div>
               <div>ПРОСТО</div>
+              <div>ОТПРАВЬ НАМ</div>
+              <div>ВЕСТОЧКУ</div>
+              {/* <div>ПРОСТО</div>
               <div>ОТПРАВЬ НАМ</div>
               <div>ВЕСТОЧКУ,</div>
               <div>ЧТО ХОЧЕШЬ</div>
-              <div>ПОГОВОРИТЬ</div>
+              <div>ПОГОВОРИТЬ</div> */}
             </div>
             <div className={style.blockFrom}>
-              <p>
-                ТЫ ПРАВИЛЬНО ПОНЯЛ, СВОИ ДАННЫЕ НУЖНО ВВЕСТИ НИЖЕ
-              </p>
+              <p>ТЫ ПРАВИЛЬНО ПОНЯЛ, СВОИ ДАННЫЕ НУЖНО ВВЕСТИ НИЖЕ</p>
               <Form />
             </div>
           </div>
